4149f6fd317d160c1d29edadff3df9eeb388cafa
Merajuddin11 <163775105+Merajuddin11@users.noreply.github.com>
2024-05-08 18:01:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js        
+var typed = new Typed(".multiple-text", {
+    strings: ["Student", "Web Developer", "Youtuber"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
